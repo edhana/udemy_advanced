d39d79c2a24ebc6a5349c7b5ba94bc250ae280fd
Eduardo Marques <edhana@gmail.com>
2017-07-29 08:58:16 -0300

    Added a test to count the multiple lines of a file

--- session_3_files/session3_files.cpp
+++ session_3_files/session3_files.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include "gtest/gtest.h"
 using namespace std;
 
@@ -24,6 +25,29 @@ TEST(WritingTextFile , Ex1CreateNewFileAndWriteInside) {
         FAIL();
     }
 
+}
+
+TEST(WritingTextFile, Ex1ExtendedCreateNewFileWithMultipleTextLines) {
+    fstream outFile;
+    outFile.open(BASE_FILENAME, ios::out);
+
+    if(outFile.is_open()) {
+        outFile << "Line 1" << endl;
+        outFile << "Line 2" << endl;
+        outFile << "Line 3" << endl;
+        outFile.close();
+    }
+    else {
+        cout << "Could not create file: " << BASE_FILENAME << endl;
+        FAIL();
+    }
+
+    // Openning the file again for reading
+    ifstream inFile(BASE_FILENAME);
+    int lineCount = count(istreambuf_iterator<char>(inFile), istreambuf_iterator<char>(), '\n');
+    EXPECT_EQ(lineCount, 3);
+
+    SUCCEED();
 }
 
 int main(int argc, char * argv[]) {
