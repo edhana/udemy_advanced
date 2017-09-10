8cb19407b4e30c33e71612fa1e12d22b6aff8e31
Eduardo Marques <edhana@gmail.com>
2017-09-10 15:17:37 -0300

    Refactored the code to add the experimental filesystem library to remove the test fixture files

--- session_3_files/session3_files.cpp
+++ session_3_files/session3_files.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-// #include <experimental/filesystem> // aha! C++17 feature from boost
+#include <experimental/filesystem> // aha! C++17 feature from boost
 #include "gtest/gtest.h"
 using namespace std;
-// namespace fs = std::experimental::filesystem;
+namespace fs = std::experimental::filesystem;
 
 const string BASE_FILENAME = "text.txt";
 const string BASE_READ_FILENAME = "text_2.txt";
@@ -13,13 +13,7 @@ class WritingTextFileLecture6 : public ::testing::Test {
 public:
     void TearDown() {
         // Remove the test file
-        fstream testFile(BASE_FILENAME);
-        cout << "Inside the fixture" << endl;
-
-        if(testFile.good()) {
-            // File exists, remove it
-            // fs::remove(BASE_FILENAME);
-        }
+        fs::remove(BASE_FILENAME);
     }
 };
 
@@ -28,25 +22,22 @@ public:
     void SetUp() {
         ofstream outFile;
 
-        //FIXME: Create the file just once
         outFile.open(BASE_READ_FILENAME);
 
         if(outFile.is_open()) {
             for(int i = 1; i <= 10; i++) {
-                outFile << "Test line " << i << endl; 
+                outFile << "Test line " << i << endl;
             }
             outFile.close();
         }
     }
+
+    //TODO: Add tear down
 };
 
-TEST(WritingTextFileLecture6, Ex1CreateNewFileAndWriteInside) {
+TEST_F(WritingTextFileLecture6, Ex1CreateNewFileAndWriteInside) {
     fstream outFile;
-    // ofstream outFile;
-
-    // string outputFileName = "text.txt";
     outFile.open(BASE_FILENAME, ios::out);
-    // outFile.open(outputFileName);
 
     if(outFile.is_open()) {
         outFile << "Hello there" << endl;
@@ -61,7 +52,7 @@ TEST(WritingTextFileLecture6, Ex1CreateNewFileAndWriteInside) {
 
 }
 
-TEST(WritingTextFileLecture6, Ex1ExtendedCreateNewFileWithMultipleTextLines) {
+TEST_F(WritingTextFileLecture6, Ex1ExtendedCreateNewFileWithMultipleTextLines) {
     fstream outFile;
     outFile.open(BASE_FILENAME, ios::out);
 
